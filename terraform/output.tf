56fe82257561bd81ea2badf4f1a6454da495de78
mitul-121 <pipaliyamitul12@gmail.com>
2026-04-27 11:24:27 +0000

    Created CI/CD pipeline to create infrastrucutre

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,17 +1,8 @@
-
-#output "ec2_public_ip" {
- # value = aws_instance.app.public_ip  
-#}
-
-
-output "ec2_public_ip" {
-  value = aws_instance.dee_store.public_ip
+output "public_ip" {
+  value       = aws_instance.dee_store.public_ip
+  description = "Public IP address of the EC2 instance"
 }
 
-output "ssh_command" {
-  value = "ssh -i ~/.ssh/dee-store-key.pem ec2-user@${aws_instance.dee_store.public_ip}"
-}
-
-output "cloudwatch_log_group_app" {
-  value = aws_cloudwatch_log_group.app_logs.name
+output "instance_id" {
+  value = aws_instance.dee_store.id
 }
